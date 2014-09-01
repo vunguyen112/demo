d9e3d883d41bc737d2e5acaa6aea9098f5ceb830
vunguyen112 <nnvu92@gmail.com>
2014-09-01 19:37:11 +0700

    Add comment....

--- src/com/example/exampletextviewcustombackground/MainActivity.java
+++ src/com/example/exampletextviewcustombackground/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends Activity  {
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Add comment.
         Log.v("V", "A");
         ListView lv=(ListView) findViewById(R.id.lvMenu);
         String[] list=getResources().getStringArray(R.array.listViewMenuSource);
